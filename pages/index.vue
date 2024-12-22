a22a44d6127549ae21ff7e764b1cf7b3b3b212a6
Levi <l.vogel@avetica.nl>
2024-12-22 16:37:04 +0100

    Cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,7 @@
 <template>
     <div class="landing">
-        <div class="content">
-            <div class="welcome-text">
-                <h1>Cool Lorem</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sapiente deserunt voluptate, nulla rem ad eligendi quidem laudantium quisquam magni provident minus repudiandae, vitae pariatur quasi dignissimos ut porro, sequi dolor exercitationem repellat quibusdam. Ipsum, vel fugiat inventore adipisci exercitationem sequi quidem minus deleniti numquam, aliquam culpa delectus eveniet. Accusantium?</p>
-            </div>
-        </div>
-        <!-- <div class="horizontal-grid">
-            <div class="line first"></div>
-            <div class="line second"></div>
-            <div class="line third"></div>
-            <div class="line fourth"></div>
-            <div class="line fifth"></div>
-        </div>
-        <div class="vertical-grid">
-            <div class="line first"></div>
-            <div class="line second"></div>
-            <div class="line third"></div>
-            <div class="line fourth"></div>
-        </div> -->
     </div>
-    <div class="content-section">
+    <div class="projects-section">
         Project view
         <div class="bento-grid">
             <div class="card"></div>

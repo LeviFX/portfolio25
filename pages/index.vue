d9100db4ed3f0098ea0239cefee46f73cfefcbec
Levi <l.vogel@avetica.nl>
2024-12-29 16:02:55 +0100

    Start bento grid & blog structure

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,56 @@
     <div class="projects-section">
         Project view
         <div class="bento-grid">
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
+            <div class="card large">card</div>
+            <div class="card">card</div>
+            <div class="card">card</div>
+            <div class="card vertical">card</div>
+            <div class="card">card</div>
+            <div class="card">card</div>
+            <div class="card horizontal">card</div>
+            <div class="card">card</div>
         </div>
+    </div>
+    <div class="blog-section">
+        Articles
+        <div class="blog-grid">
+            <div class="card">
+                <div class="blog-content">
+                    <h1 class="blog-title">
+                        Blog 1
+                    </h1>
+                    <span class="blog-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam distinctio quidem rem, repudiandae ullam accusamus
+                    </span>
+                </div>
+                <img src="assets/img/cover1.png" class="blog-thumbnail" alt="">
+            </div>
+            <div class="card">
+                <div class="blog-content">
+                    <h1 class="blog-title">
+                        Blog 2
+                    </h1>
+                    <span class="blog-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam distinctio quidem rem, repudiandae ullam accusamus
+                    </span>
+                </div>
+                <img src="assets/img/cover2.png" class="blog-thumbnail" alt="">
+            </div>
+            <div class="card">
+                <div class="blog-content">
+                    <h1 class="blog-title">
+                        Blog 3
+                    </h1>
+                    <span class="blog-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam distinctio quidem rem, repudiandae ullam accusamus
+                    </span>
+                </div>
+                <img src="assets/img/cover1.png" class="blog-thumbnail" alt="">
+            </div>
+
+        </div>
+    </div>
+    <div class="contact-section">
+        Contact
     </div>
 </template>

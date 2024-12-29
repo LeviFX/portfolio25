414ea468fa0b099beb5fd62c3268e5c95e2d30ba
Levi <l.vogel@avetica.nl>
2024-12-29 16:50:50 +0100

    Bento card content + styling + dummy img's

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,78 @@
     <div class="projects-section">
         Project view
         <div class="bento-grid">
-            <div class="card large">card</div>
-            <div class="card">card</div>
-            <div class="card">card</div>
-            <div class="card vertical">card</div>
-            <div class="card">card</div>
-            <div class="card">card</div>
-            <div class="card horizontal">card</div>
-            <div class="card">card</div>
+            <div class="card large">
+                <div class="category">
+                    #3d
+                </div>
+                <div class="project-description">
+                    Lorem ipsum dolor sit.
+                </div>
+                <img src="assets/img/animationkeyboard.webp" class="project-thumbnail" alt="">
+            </div>
+            <div class="card">
+                <div class="category">
+                    #dev
+                </div>
+                <div class="project-description">
+                    Lorem ipsum dolor sit.
+                </div>
+                <img src="assets/img/astronaut.webp" class="project-thumbnail" alt="">
+            </div>
+            <div class="card">
+                <div class="category">
+                    #design
+                </div>
+                <div class="project-description">
+                    Lorem ipsum dolor sit.
+                </div>
+                <img src="assets/img/cathedral.webp" class="project-thumbnail" alt="">
+            </div>
+            <div class="card vertical">
+                <div class="category">
+                    #3d #dev
+                </div>
+                <div class="project-description contrast">
+                    Lorem ipsum dolor sit.
+                </div>
+                <img src="assets/img/remia_small.webp" class="project-thumbnail" alt="">
+            </div>
+            <div class="card">
+                <div class="category">
+                    #dev
+                </div>
+                <div class="project-description">
+                    Lorem ipsum dolor sit.
+                </div>
+                <img src="assets/img/sgp3d.webp" class="project-thumbnail" alt="">
+            </div>
+            <div class="card">
+                <div class="category">
+                    #motion
+                </div>
+                <div class="project-description">
+                    Lorem ipsum dolor sit.
+                </div>
+                <img src="assets/img/remia_small.webp" class="project-thumbnail" alt="">
+            </div>
+            <div class="card horizontal">
+                <div class="category">
+                    #test
+                </div>
+                <div class="project-description">
+                    Lorem ipsum dolor sit.
+                </div>
+                <img src="assets/img/twirl.webp" class="project-thumbnail" alt="">
+            </div>
+            <div class="card">
+                <div class="category">
+                    #dev
+                </div>
+                <div class="project-description">
+                    Lorem ipsum dolor sit.
+                </div>
+                <img src="assets/img/chess.webp" class="project-thumbnail" alt="">
+            </div>
         </div>
     </div>
     <div class="blog-section">

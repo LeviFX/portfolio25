09a3f253f353f88ddfdb810695f225e29281e300
Levi <levipostvak@protonmail.com>
2025-01-14 20:48:30 +0100

    Better description

--- components/characterRender.vue
+++ components/characterRender.vue
@@ -37,7 +37,7 @@ const init = () => {
     // Create Scene
     scene = new THREE.Scene();
 
-    // Add group
+    // Add group, lookAt() somehow only works when the model is in a group
     scene.add(group);
 
     // Create Camera

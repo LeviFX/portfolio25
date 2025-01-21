794321e0a0ca9bcd406dd42e24bbca0de4958670
Levi <levipostvak@protonmail.com>
2025-01-21 22:24:16 +0100

    Start of blog page + gradient blob changes

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,3 +1,30 @@
 <template>
-    <h1>blog</h1>
+    <div class="blogs-page">
+        <div class="blogs-section">
+            <div class="blogs-featured">
+            <div class="card">
+                <h1 class="title">blabla</h1>
+                <div class="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis!</div>
+                <img src="/img/cover1.png" class="blog-thumbnail" alt="">
+            </div>
+        </div>
+        <div class="blogs-other">
+            <div class="card">
+                <h1 class="title">card 1</h1>
+                <div class="description">Lorem ipsum dolor sit amet.</div>
+                <img src="/img/cover1.png" class="blog-thumbnail" alt="">
+            </div>
+            <div class="card">
+                <h1 class="title">card 2</h1>
+                <div class="description">Lorem ipsum dolor, sit amet consectetur adipisicing.</div>
+                <img src="/img/cover1.png" class="blog-thumbnail" alt="">
+            </div>
+            <div class="card">
+                <h1 class="card3">card 3</h1>
+                <div class="description">Lorem, ipsum dolor.</div>
+                <img src="/img/cover1.png" class="blog-thumbnail" alt="">
+            </div>
+        </div>
+        </div>
+    </div>
 </template>

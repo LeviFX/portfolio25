0e5a7502052297340dad88648941ebd7659e9ce6
Levi <l.vogel@avetica.nl>
2025-03-08 12:24:25 +0100

    Blog content styling

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,10 +1,25 @@
 <script lang="ts" setup>
-const route = useRoute()
-const { data: page } = await useAsyncData(route.path, () => {
-  return queryCollection('blog').path(route.path).first()
-})
+
+  const route = useRoute()
+  const { data: blogData } = await useAsyncData(route.path, () => {
+    return queryCollection('blog').path(route.path).first()
+  })
+
+  useHead({
+    bodyAttrs: {
+      class: 'blog-page'
+    }
+  })
+
 </script>
 
-<template>
-  <ContentRenderer v-if="page" :value="page" />
+<template v-if="blogData">
+  <div class="blog-content">
+    <div class="blog-banner">
+      <img :src="blogData.image" alt="">
+    </div>
+    <div class="content">
+      <ContentRenderer :value="blogData"/>
+    </div>
+  </div>
 </template>
